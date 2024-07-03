237b81ce375871f39d82d2888f2385aedb124512
Moveszone <biswanathm973@gmail.com>
2024-07-03 18:58:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -104,7 +104,7 @@ else:
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
 URL = "https://civilian-bonobo-moveszone-a3e12fc9.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "http://civilian-bonobo-moveszone-a3e12fc9.koyeb.app/".format(FQDN, PORT)
+    "https://civilian-bonobo-moveszone-a3e12fc9.koyeb.app/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -121,7 +121,7 @@ HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
     URL = "https://civilian-bonobo-moveszone-a3e12fc9.koyeb.app/".format(FQDN)
 else:
-    URL = "http://civilian-bonobo-moveszone-a3e12fc9.koyeb.app/".format(FQDN)
+    URL = "https://civilian-bonobo-moveszone-a3e12fc9.koyeb.app/".format(FQDN)
 BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001987654567")).split())) 
 OWNER_USERNAME = "LetsTryBM"
 
